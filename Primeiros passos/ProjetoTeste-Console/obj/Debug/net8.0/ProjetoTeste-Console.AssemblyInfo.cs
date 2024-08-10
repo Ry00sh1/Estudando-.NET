8ff171633bd5d5f63abbb8f2426420f9ab6f1d5b
Pedro <phasena@gmail.com>
2024-08-10 20:04:04 -0300

    correção

--- Primeiros passos/ProjetoTeste-Console/obj/Debug/net8.0/ProjetoTeste-Console.AssemblyInfo.cs
+++ Primeiros passos/ProjetoTeste-Console/obj/Debug/net8.0/ProjetoTeste-Console.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoTeste-Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6d006595cc9586672152bec777950af8569ceb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eaf6b2dd996fa831f00a20e37b20433ae09b162f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoTeste-Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoTeste-Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
